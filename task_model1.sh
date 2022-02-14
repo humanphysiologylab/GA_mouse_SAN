5a18750f7779800c884fd10db20edfc13000c4b9
Dmitry Rybashlykov <rybashlykov@h1.hpc>
2022-02-14 12:56:49 +0300

    extended boundaries in model2

--- task_model1.sh
+++ task_model1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --time 12:00:00
 #SBATCH --partition normal
-#SBATCH -n64 -N4
+#SBATCH -n128 -N8
 #SBATCH --job-name gonotkov_ga_fit
 #SBATCH --comment pacemaker_genetic_algorithms
 
