65644467968de54ed14fc07aeb77922d186727ec
Dmitry Rybashlykov <rybashlykov@h1.hpc>
2022-02-14 14:50:30 +0300

    tasks for new configs

---
+++ task_model_29_09_161.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --time 12:00:00
+#SBATCH --partition normal
+#SBATCH -n128 -N8
+#SBATCH --job-name gonotkov_ga_fit
+#SBATCH --comment pacemaker_genetic_algorithms
+
+cd /data90t/users/rybashlykov/new_GA_lsoda/bondarenko
+
+mpiexec python mpi_script.py /data90t/users/rybashlykov/new_GA_lsoda/bondarenko/configs/pacemaker_model_29_09_161.json
